9136e661743a291705bd2677ab648cc794aaf0ba
Marisa Ramon <mer0522@gmail.com>
2016-02-25 22:06:39 -0600

    Add config.action_mailer to production environment with heroku url as host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,6 +4,10 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :test
+  host = 'https://murmuring-castle-71999.herokuapp.com/'
+  config.action_mailer.default_url_options = { host: host }
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
