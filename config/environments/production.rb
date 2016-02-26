27d794e9be9efde1bd906cea5cc4dca2baf6e3b0
Marisa Ramon <mer0522@gmail.com>
2016-02-25 22:29:08 -0600

    Edit production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,7 +5,7 @@ Rails.application.configure do
   config.cache_classes = true
 
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :test
+  config.action_mailer.delivery_method = :production
   host = 'https://murmuring-castle-71999.herokuapp.com/'
   config.action_mailer.default_url_options = { host: host }
   # Eager load code on boot. This eager loads most of Rails and
